743aa917311674d9716e614c49e61d44d6e60a36
RivaldoCM <rivaldocardoso2010@hotmail.com>
2023-10-04 01:37:21 -0300

    test: testado sem sucesso

--- src/services/apiVoalle/apiVoallePortal.ts
+++ src/services/apiVoalle/apiVoallePortal.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const getToken = async () => {
-    await axios.get(`http://api.acesse.net.br/portal_authentication?`, {
+    await axios.get(`${import.meta.env.VITE_BASE_URL_PORTAL_VOALLE}/portal_authentication?`, {
         params:{
-            verify_token: 'TWpNMU9EYzVaakk1T0dSaU1USmxaalprWldFd00ySTFZV1JsTTJRMFptUT06V2tkS2JHSllRWGROUkUwMVRuYzlQUT09OlpUaGtNak0xWWprMFl6bGlORE5tWkRnM01EbGtNalkyWXpBeE1HTTNNR1U9',
-            client_id: '9_4qqfdftepfcw0oc0gs4kcgswoks4k0ksggksckwkgocgckg4cg',
-            client_secret: '3xmnp6ao70yssww0kcggg4okoc4oo0cgs4gkkcgc8ook8ko0sg',
+            verify_token: import.meta.env.VITE_PORTAL_TOKEN,
+            client_id: import.meta.env.VITE_CLIENT_ID_PORTAL_VOALLE,
+            client_secret: import.meta.env.VITE_CLIENT_SECRET_PORTAL_VOALLE,
             grant_type: 'client_credentials',
             username: '12748829662',
             password: '12748829662',
