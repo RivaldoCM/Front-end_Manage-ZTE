9490200713c247fe6f3a588f8363113a9def5892
RivaldoCM <rivaldocardoso2010@hotmail.com>
2023-10-06 12:17:07 -0300

    test: testando integração com API TP.

--- src/services/apiVoalle/apiVoallePortal.ts
+++ src/services/apiVoalle/apiVoallePortal.ts
@@ -1,16 +1,10 @@
 import axios from "axios";
 
 export const getToken = async () => {
-    await axios.get(`${import.meta.env.VITE_BASE_URL_PORTAL_VOALLE}/portal_authentication?`, {
-        params:{
-            verify_token: import.meta.env.VITE_PORTAL_TOKEN,
-            client_id: import.meta.env.VITE_CLIENT_ID_PORTAL_VOALLE,
-            client_secret: import.meta.env.VITE_CLIENT_SECRET_PORTAL_VOALLE,
-            grant_type: 'client_credentials',
-            username: '12748829662',
-            password: '12748829662',
-        }
+    await axios.post('http://localhost:4000/getDataClient', {
+        peopleID: '134473'
     })
+
     .then(response => {
         console.log(response.data)
     })
---
+++ src/services/apiVoalle/apiVoalleThirdParty.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export function getToken(){
+    return new Promise( async (resolve, reject) => {
+        await axios.post(`${import.meta.env.VITE_BASEURL_TP}:4500/connect/token`, {
+            grant_type: 'client_credentials',
+            scope: 'syngw',
+            client_id: import.meta.env.VITE_CLIENTID_TP,
+            client_secret: import.meta.env.VITE_CLIENTSECRET_TP,
+            syndata: import.meta.env.VITE_VERIFYTOKEN_TP
+        }).then(response => {
+            console.log(response);
+            resolve(response)
+        }).then(err => {
+            console.log(err);
+        });
+    });
+}
