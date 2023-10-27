df1c59471cdf5532d8951aedc9ace2497070eb05
RivaldoCM <rivaldocardoso2010@hotmail.com>
2023-10-27 19:57:13 -0300

    fix: Corrigindo headers da requisição

--- src/services/apiVoalle/apiVoalleThirdParty.ts
+++ src/services/apiVoalle/apiVoalleThirdParty.ts
@@ -3,9 +3,10 @@ import axios from "axios"
 export const getToken = async () => {
     await axios.post(`${import.meta.env.VITE_BASEURL_TP}:45700/connect/token`, {
         grant_type: 'client_credentials',
-        verify_token: import.meta.env.VITE_VERIFYTOKEN_TP,
+        scope: 'syngw',
         client_id: import.meta.env.VITE_CLIENTID_TP,
         client_secret: import.meta.env.VITE_CLIENTSECRET_TP,
+        syndata: import.meta.env.VITE_VERIFYTOKEN_TP
     })
     .then(response => {
         console.log(response.data)
