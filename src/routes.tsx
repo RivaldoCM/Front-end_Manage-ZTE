6239dc7fecba152b7c404cb2427d10ed72091ef5
RivaldoCM <rivaldocardoso2010@hotmail.com>
2023-11-29 02:10:14 -0300

    chore: Adiciobnando telaa lidar usuarios.

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { Provisionamento } from "./pages/Provisionamento";
 import { HandleManageOlt } from "./pages/admin/manageOlt";
+import { HandleManageUsers } from "./pages/admin/users";
 
 export function AppRoutes(){
     return (
         <Routes>
             <Route path="/" element={ <Provisionamento /> } />
             <Route path="/admin" element={ <HandleManageOlt /> } />
+            <Route path="/users" element={ <HandleManageUsers />} />
         </Routes>
     )
 };
