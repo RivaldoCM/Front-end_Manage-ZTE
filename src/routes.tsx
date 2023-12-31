20d0c9dce9c4491e47d4b2d442073e6e3b200fa9
RivaldoCM <rivaldocardoso2010@hotmail.com>
2023-12-30 23:38:42 -0300

    fix: Corrigido importações de arquivos

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,7 +9,7 @@ import { HandleManageOlt } from "./pages/admin/manageOlt";
 import { HandleManageUsers } from "./pages/admin/users";
 import { MenuDrawer } from "./components/DesktopMenu";
 import { MobileDrawerMenu } from "./components/MobileMenu";
-import { OnuDelete } from "./pages/OnuDelete";
+import { OnuDelete } from "./pages/onuDelete";
 
 interface PrivateRouteProps {
     element: ReactElement;
