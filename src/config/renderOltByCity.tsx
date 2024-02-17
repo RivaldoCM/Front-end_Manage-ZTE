ef32d182acf573dd107de07cb8ff58ca174a7fcd
RivaldoCM <rivaldocardoso2010@hotmail.com>
2024-02-17 11:03:26 -0300

    fix: Tipagem de dados para atualizar olt

--- src/config/renderOltByCity.tsx
+++ src/config/renderOltByCity.tsx
@@ -5,7 +5,6 @@ import { IOlt } from "../interfaces/IOlt";
 export const handleOltByCity = (olts: IOlt[] | undefined) => {  
     const cityIds = new Set<number>();
     const oltNames = new Set<string>();
-
     if(olts){
         return olts.map((olt, index: number) => {
             if(!cityIds.has(olt.city_id) && !oltNames.has(olt.name)){
@@ -16,11 +15,17 @@ export const handleOltByCity = (olts: IOlt[] | undefined) => {
                     <MenuItem key={index} value={olt.name}>
                         {olt.name}
                     </MenuItem>
-                )
+                );
             } else {
                 //OLT's DA MESMA REGIÃO POREM EM LOCAIS DIFERENTES
-                let match = olt.name.match(/([a-zA-Z]+\s+[a-zA-Z]+).*/);
+                let match = olt.name.match(/([a-zA-Z]+).*/);
+
+                if(match){
+                    console.log(match[1].includes(olt.name), olt.name)
+                }
+
                 for(let name of oltNames){
+
                     if(match && !name.includes(match[1]) && !oltNames.has(match[1])){
                         return(
                             <MenuItem key={index} value={match[1]}>
